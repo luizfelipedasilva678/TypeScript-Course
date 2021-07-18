db37643a719432a547ba4a8cf5dba5afda8eabf8
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-07-17 21:47:59 -0300

    literal types

---
+++ src/type-annotation/literal-types.ts
@@ -0,0 +1,18 @@
+let x = 10; // eslint-disable-line
+x = 0b1010;
+const y = 10;
+
+let a = 100 as const; // eslint-disable-line
+
+const pessoa = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+};
+
+function escolhaCor(cor: 'Vemelho' | 'Amarelo' | 'Azul') {
+  return cor;
+}
+
+console.log(escolhaCor);
+
+export default 1;
