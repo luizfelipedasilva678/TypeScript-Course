6d2fd43bf3786082b90973020449e19b6c3ac920
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-07-18 00:08:28 -0300

    alias

---
+++ src/type-annotation/type-alias.ts
@@ -0,0 +1,23 @@
+type Idade = number;
+
+type Pessoa = {
+  nome: string;
+  idade: Idade;
+  salario: number;
+  corPreferida?: string;
+};
+
+type CorRGB = 'Vermelho' | 'Verde' | 'Azul';
+type CMYK = 'Ciano' | 'Magenta' | 'Amarela' | 'Preta';
+
+const pessoa: Pessoa = {
+  nome: 'Teste',
+  idade: 30,
+  salario: 15000,
+};
+
+export function setCorPreferida(pessoa: Pessoa, cor: corPreferida) {
+  return { ...pessoa, corPreferida: cor };
+}
+
+console.log(setCorPreferida(pessoa, ''));
