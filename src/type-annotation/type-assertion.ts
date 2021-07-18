4d7ac3615c52ce25ed9f04ab548d7b577a09aa91
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-07-18 14:27:08 -0300

    assertion type

---
+++ src/type-annotation/type-assertion.ts
@@ -0,0 +1,8 @@
+const body = document.querySelector('body');
+if (body) body.style.background = 'red';
+
+const body2 = document.querySelector('body')!;
+body2.style.background = 'red';
+
+const body3 = document.querySelector('body') as HTMLBodyElement;
+body3.style.background = 'red';
