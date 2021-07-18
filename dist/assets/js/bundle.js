544f2609af647ba1349c718a6213896af2aec130
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-07-18 14:45:51 -0300

    webpack config

---
+++ dist/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/webpack/index.ts ***!
+  \******************************/
+
+console.log('Olá');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
