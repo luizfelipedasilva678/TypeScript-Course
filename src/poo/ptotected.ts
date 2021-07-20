a3d565f6cbfa81f0208f4731a0c107c3db434020
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-07-19 21:07:21 -0300

    protected

---
+++ src/poo/ptotected.ts
@@ -0,0 +1,50 @@
+export class Empresa {
+  public readonly nome: string;
+  protected readonly colaboradores: Colaborador[] = [];
+  private readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Udemy extends Empresa {
+  constructor() {
+    super('Udemy', '12121212/2312321-123');
+  }
+
+  popColaborador(): Colaborador | null {
+    const colaborador = this.colaboradores.pop();
+    if (colaborador) return colaborador;
+    return null;
+  }
+}
+
+export class Colaborador {
+  constructor(public readonly nome: string) {
+    this.nome = nome;
+  }
+}
+
+const empresa1 = new Udemy();
+const colaborador1 = new Colaborador('José');
+const colaborador2 = new Colaborador('José');
+const colaborador3 = new Colaborador('José');
+const colaborador4 = new Colaborador('José');
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+empresa1.adicionaColaborador(colaborador4);
+
+console.log(empresa1.mostrarColaboradores());
