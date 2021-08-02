7c8ad27d4dd26f5079c36ba20970650dcb41d0cf
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-08-01 23:39:33 -0300

    interface

---
+++ src/poo/interface.ts
@@ -0,0 +1,24 @@
+interface TipoNome {
+  nome: string;
+}
+
+interface TipoSobrenome {
+  sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+  nomeCompleto: () => string;
+}
+
+interface TipoPessoa2 extends TipoNome, TipoSobrenome {}
+
+export class Pessoa implements TipoNome, TipoSobrenome, TipoNomeCompleto {
+  constructor(public nome: string, public sobrenome: string) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('Luiz', 'Miranda');
+console.log(pessoa.nomeCompleto());
