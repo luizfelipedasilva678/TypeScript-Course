2c37cf82c25fdd7535a0198d96c393bc5a31a65f
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-08-15 00:14:14 -0300

    utilities

---
+++ src/generics/utility.ts
@@ -0,0 +1,58 @@
+const objeto1: Record<string, string | number> = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+  idade: 30,
+};
+console.log(objeto1);
+
+type PessoaProtocol = {
+  nome?: string;
+  sobrenome?: string;
+  idade?: number;
+};
+
+type PessoaRequired = Required<PessoaProtocol>;
+type PessoaPartial = Partial<PessoaRequired>;
+type PessoaReadonly = Readonly<PessoaRequired>;
+type PessoaPick = Pick<PessoaRequired, 'nome' | 'sobrenome'>;
+
+const objeto2: PessoaRequired = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+  idade: 30,
+};
+console.log(objeto2);
+
+type ABC = 'A' | 'B' | 'C';
+type CDE = 'C' | 'D' | 'E';
+type TipoExclude = Exclude<ABC, CDE>;
+type TipoExtract = Extract<ABC, CDE>;
+
+type AccountMongo = {
+  _id: string;
+  nome: string;
+  idade: number;
+  sobrenome: string;
+};
+
+type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
+  id: string;
+};
+
+const accountMongo: AccountMongo = {
+  _id: 'asfd9p8a7sdf90a8s76f9as',
+  nome: 'Luiz',
+  idade: 30,
+  sobrenome: 'Miranda',
+};
+
+function mapAccount(accountMongo: AccountMongo): AccountApi {
+  const { _id, ...accountData } = accountMongo;
+  return { ...accountData, id: _id };
+}
+
+const accountApi = mapAccount(accountMongo);
+console.log('API:');
+console.log(accountApi);
+
+export default 1;
