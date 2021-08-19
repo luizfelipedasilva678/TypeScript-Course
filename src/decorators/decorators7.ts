4a597ebe39f36533a6c8321440d7be3a895c605c
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-08-18 23:37:13 -0300

    decorators de propriedades

---
+++ src/decorators/decorators7.ts
@@ -0,0 +1,33 @@
+function decorador(classPrototype: any, nome: string | symbol): any {
+  console.log(classPrototype);
+  console.log(nome);
+}
+
+export class UmaPessoa {
+  @decorador
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
